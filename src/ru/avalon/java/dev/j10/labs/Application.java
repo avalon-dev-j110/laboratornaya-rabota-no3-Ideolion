1554762f9b9890cdad025ad2bccd8e6a3da95254
Ufilindv <>
2020-11-02 17:33:22 +0300

    Lab_1_done

--- src/ru/avalon/java/dev/j10/labs/Application.java
+++ src/ru/avalon/java/dev/j10/labs/Application.java
@@ -1,37 +1,69 @@
 package ru.avalon.java.dev.j10.labs;
 
+import ru.avalon.java.dev.j10.labs.initialization.FibonacciInitializer;
+import ru.avalon.java.dev.j10.labs.initialization.RandomInitializer;
+import ru.avalon.java.dev.j10.labs.sort.BubbleSort;
+import ru.avalon.java.dev.j10.labs.sort.SelectionSort;
+import ru.avalon.java.dev.j10.labs.sort.ShellSort;
+
 public class Application {
 
     public static void main(String[] args) {
-        int[] array;
 
 	    /*
 	     * TODO(Студент): Выполнить действия над массивом чисел
 	     *
 	     * 0. Инициализировать переменную array массивом из 20 целых чисел.
-	     *
-	     * 1. Проинициализировать массив значениями
+	     */
+    int[] array = new int[20];
+            
+	    /* 1. Проинициализировать массив значениями
 	     *    последовательности чисел Фибоначчи.
-	     *
-	     * 2. Найти сумму элементов массива.
-	     *
-	     * 3. Проинициализировать массив последовательностью
+	     */
+    FibonacciInitializer fibonacciInitializer = new FibonacciInitializer();
+    fibonacciInitializer.initialize(array);
+          
+        /* 2. Найти сумму элементов массива.
+         */
+    int sumfib = 0;
+    for (int i = 0; i < array.length; i++){
+        sumfib =sumfib + array[i];
+        }
+      
+	     /* 3. Проинициализировать массив последовательностью
 	     *    случайных чисел в диапазоне от -50 до 50.
-	     *
-	     * 4. Отсортировать массив с использованием
+	     */
+    RandomInitializer randomInitializer = new RandomInitializer(-50, +50);
+	randomInitializer.initialize(array);
+          
+	     /* 4. Отсортировать массив с использованием
 	     *    пузырьковой сортировки.
-	     *
-         * 5. Проинициализировать массив последовательностью
+	     */
+
+    BubbleSort buble = new BubbleSort();
+	buble.sort(array);
+                 
+         /* 5. Проинициализировать массив последовательностью
          *    случайных чисел в диапазоне от -50 до 50.
-         *
-         * 6. Отсортировать массив с использованием
+         */
+    RandomInitializer randomInitializer2 = new RandomInitializer(-50, +50);
+        randomInitializer.initialize(array);
+         /* 6. Отсортировать массив с использованием
          *    сортировки выбором.
-         *
-         * 7. Проинициализировать массив последовательностью
+         */
+    SelectionSort selection = new SelectionSort();
+        selection.sort(array);
+     
+         /* 7. Проинициализировать массив последовательностью
          *    случайных чисел в диапазоне от -50 до 50.
-         *
-         * 8. Отсортировать массив с использованием
+         */
+    RandomInitializer randomInitializer3 = new RandomInitializer(-50, +50);
+	randomInitializer.initialize(array);
+        /* 8. Отсортировать массив с использованием
          *    сортировки Шелла.
 	     */
+    ShellSort shell = new ShellSort();
+        shell.sort(array);
+         
     }
 }
